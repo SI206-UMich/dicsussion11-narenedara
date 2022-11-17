3795ddbe67e4ecaabe37a80b059cc636bd6b252c
narenedara <112345257+narenedara@users.noreply.github.com>
2022-11-16 23:10:35 -0500

    1

--- discussion11.py
+++ discussion11.py
@@ -39,11 +39,17 @@ def create_species_table(cur, conn):
 # TASK 1
 # CREATE TABLE FOR PATIENTS IN DATABASE
 def create_patients_table(cur, conn):
+    cur.execute("DROP TABLE IF EXISTS patients")
+    cur.execute("CREATE TABLE Patients(pet_id INTEGER PRIMARY KEY, name TEXT, species_id NUMBER, age INTEGER, cuteness INTEGER, aggressiveness NUMBER")
+    conn.commit()
+
     pass
 
 
 # ADD FLUFFLE TO THE TABLE
 def add_fluffle(cur, conn):
+    cur.execute("INSERT INTO Patients (pet_id, name, species_id, age, cuteness, VALUES (?,?,?,?,?,?)",(0,"Fluffle", 0,3,90,100))
+    conn.commit()
     pass
     
 
